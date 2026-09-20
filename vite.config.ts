ad269412c90640ebebf61e273465f96bb0c9fde2
SharivyaJewellery <sharivyajewellery@gmail.com>
2026-09-20 18:26:56 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/manipal-donne-briyani/',
+    base: '/manipal-donne-biriyani/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
